37ae42338b1202c4dc8dc3289fc4002cd5b0cbb8
Anton Khanabeev <khanabeev@gmail.com>
2020-12-22 19:13:16 +0300

    major fix

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Khanabeev/testmodule
+module github.com/Khanabeev/testmodule/v2
 
 go 1.15
--- main.go
+++
@@ -1,8 +0,0 @@
-package testmodule
-
-import "fmt"
-
-func Hi(name string) string {
-	//return fmt.Sprintf("Hi, %s", name)
-	return fmt.Sprintf("Hi, %s!", name)
-}
